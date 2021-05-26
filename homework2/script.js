723c958d438ece506a5b234029fadf351d3072a6
Artem <kozhin345@gmail.com>
2021-05-26 15:59:21 +0300

    first commit

---
+++ homework2/script.js
@@ -0,0 +1,14 @@
+//Task_1
+let admin = 10;
+let names = 'Джон';
+
+admin = names;
+alert(admin);
+
+// Task_2
+let earth_Name = 10;
+let current_Visitor;
+
+// Task_3
+let num = '10' + 10;
+console.log(typeof num);

a4f55259a06df231b94b45b600f7f7fdc640a8ea
Artem <kozhin345@gmail.com>
2021-05-30 16:30:56 +0300

    corrected

--- HomeWork3/js/script.js
+++ HomeWork3/js/script.js
@@ -1,11 +1,11 @@
 // Task_1
 
 //Boolean(Number('10')) + 1; Выведет 2.
-//'sub' + Number(fasle); выдаст ERROR. 
-//16 * `    19`; Выведет 304.
+//'sub' + Number(fasle); выдаст sub0. 
+//16 * `    19`; Выведет 1456.
 //true - 70; Неявное преобразование true можно представить как 1 - 70 = -69.
 //Number(1 + String(1)) + 1; выведет 12.
-
+console.log(16  *  '          91      ');
 // Task_2
 
 let numHour = prompt('Введите кол-во часов: '); 
@@ -22,6 +22,6 @@ num += 12;
 num -= 14;
 num *= 5;
 num /= 7;
-num += 1;
-num -= 1;
+++num;
+--num;
 alert(num);

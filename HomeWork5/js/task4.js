8f22209e8108e31584da6cd33fcbd245acc0be37
Artem <kozhin345@gmail.com>
2021-06-20 17:41:58 +0300

    init

---
+++ HomeWork5/js/task4.js
@@ -0,0 +1,6 @@
+const arr = [3, 2, 1, 6, 2, 6, 10, 1];
+let sum = 0;
+for (let i = 0; i <= arr.length -1; i++) {
+    sum += arr[i];
+}
+console.log(sum);
---
+++ HomeWork5/js/task5.js
@@ -0,0 +1,6 @@
+const arr = [1, 2, 2];
+let sum = 0;
+for (let i = 0; i <= arr.length -1; i++) {
+    sum += arr[i] ** 2;
+}
+console.log(sum);
